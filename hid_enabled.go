ddd9ed2af58fb8ff8ebbf65d06c1d9ced9abe286
Nathan Hüsken <nathan@wintercloud.de>
2021-03-11 12:03:24 +0100

    A fix I do not understand, remove 0x00 byte for windows

--- hid_enabled.go
+++ hid_enabled.go
@@ -162,13 +162,13 @@ func (dev *Device) Write(b []byte) (int, error) {
 	if device == nil {
 		return 0, ErrDeviceClosed
 	}
-	// Prepend a HID report ID on Windows, other OSes don't need it
-	var report []byte
-	if runtime.GOOS == "windows" {
-		report = append([]byte{0x00}, b...)
-	} else {
+	// // Prepend a HID report ID on Windows, other OSes don't need it
+	// var report []byte
+	// if runtime.GOOS == "windows" {
+	//	report = append([]byte{0x00}, b...)
+	// } else {
 		report = b
-	}
+	// }
 	// Execute the write operation
 	written := int(C.hid_write(device, (*C.uchar)(&report[0]), C.size_t(len(report))))
 	if written == -1 {
